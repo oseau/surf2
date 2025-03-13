8497a62210cf636e5dfcd8f7ed82da36775ab517
yang <oseau@me.com>
2025-03-13 21:16:04 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # surf2
+
+short-cuts for https://v2.surf.one/
+
+- `left-arrow` to switch to `long`
+- `right-arrow` to `short`
+- `1` to add a position in current selected direction
+- `2` to set TP to 5%
+- `space` to close position using market price
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,70 @@
 // @ts-ignore isolatedModules
-console.log('hello world');
+
+// get div by content
+const waitForElementByXpath = (xpath: string): Promise<HTMLElement> => {
+    return new Promise((resolve) => {
+        if (document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue) {
+            return resolve(document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue as HTMLElement);
+        }
+        const observer = new MutationObserver(() => {
+            if (document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue) {
+                observer.disconnect();
+                resolve(document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue as HTMLElement);
+            }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    });
+};
+
+const bindKeys = async () => {
+    document.addEventListener('keydown', async (e) => {
+        // ignore if we are in a input field
+        if (document.activeElement instanceof HTMLInputElement) {
+            return;
+        }
+        if (e.key === 'ArrowLeft') {
+            e.preventDefault();
+            const long = await waitForElementByXpath('//div[text()="Long"]');
+            long.click();
+        }
+        else if (e.key === 'ArrowRight') {
+            e.preventDefault();
+            const short = await waitForElementByXpath('//div[text()="Short"]');
+            short.click();
+        }
+        else if (e.key === '1') { // number 1 to place order
+            const trade = await waitForElementByXpath('//button[text()="Trade"]');
+            trade.click();
+        }
+        else if (e.key === '2') { // number 2 to place order
+            e.preventDefault(); // prevent inserting 2 into input field
+            // Create a race between the two element queries to use whichever returns first
+            const [element, elType] = await Promise.race([
+                waitForElementByXpath('//div[text()="+ Add"]').then(el => [el, 'tpSl']),
+                waitForElementByXpath('(//*[starts-with(@id,"tabs")]//*[local-name() = "svg"])[2]').then(el => [el, 'edit'])
+            ]);
+            if (elType === 'tpSl') {
+                (element as HTMLElement).click();
+            } else {
+                (element as HTMLElement).dispatchEvent(new Event('click', { bubbles: true }));
+            }
+            const input = await waitForElementByXpath('(//*[starts-with(@id,"dialog")]//input)[2]') as HTMLInputElement;
+            const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+                window.HTMLInputElement.prototype,
+                'value')!.set;
+            nativeInputValueSetter!.call(input, "5");
+            const event = new Event('input', { bubbles: true });
+            input.dispatchEvent(event);
+
+            const confirm = await waitForElementByXpath('//button[text()="Confirm"]');
+            confirm.click();
+        }
+        else if (e.key === ' ') { // space to sell market
+            e.preventDefault(); // prevent scrolling
+            const market = await waitForElementByXpath('//p[text()="Market"]');
+            market.click();
+        }
+    });
+};
+
+bindKeys();
